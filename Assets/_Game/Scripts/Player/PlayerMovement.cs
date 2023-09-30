5c47d5927fa3625cea1948b352980dac380f5c68
ItsNoora <nooratarvo@gmail.com>
2023-09-30 20:21:21 +0300

    added a cooldown to dash

--- Assets/_Game/Scripts/Player/PlayerMovement.cs
+++ Assets/_Game/Scripts/Player/PlayerMovement.cs
@@ -13,6 +13,10 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField]
     private float _dashSpeed = 12f;
 
+    [SerializeField]
+    private float _dashInterval = 13f;
+
+    private float _dashTimer;
     private bool _canMove = true;
 
     private void OnEnable()
@@ -41,6 +45,7 @@ public class PlayerMovement : MonoBehaviour
             return;
         }
 
+        _dashTimer -= Time.deltaTime; 
 
         float xMove = Input.GetAxisRaw("Horizontal");
         float zMove = Input.GetAxisRaw("Vertical");
@@ -48,7 +53,7 @@ public class PlayerMovement : MonoBehaviour
         Vector3 movement = new Vector3(xMove, 0f, zMove).normalized;
         transform.rotation = Quaternion.LookRotation(-movement); // turns character model in the direction it is moving in
 
-        if (Input.GetKeyDown(KeyCode.Space)) // calls the dodge function to dodge in movement direction
+        if (Input.GetKeyDown(KeyCode.Space) && _dashTimer < 0) // calls the dodge function to dodge in movement direction
         {
             StartCoroutine(Dodge(movement));
         }
@@ -65,6 +70,7 @@ public class PlayerMovement : MonoBehaviour
             transform.Translate(movement * _dashSpeed * Time.deltaTime, Space.World); // increases movement speed for a set time
             yield return null;
         }
+        _dashTimer = _dashInterval; // resets dash timer interval (can only dash every n seconds)
     }
 
     private void DisableMovement()
